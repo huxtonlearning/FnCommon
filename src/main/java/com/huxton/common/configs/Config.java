e15ce7f7039a82055216fcefd20f93e50e953e4f
longnh <longjava2024@gmail.com>
2025-03-07 20:55:30 +0700

    fix

---
+++ src/main/java/com/huxton/common/configs/Config.java
@@ -0,0 +1,12 @@
+package com.huxton.common.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.huxton.common")
+public class Config {
+
+}

f72093611bfdaba87cbde5717c641d03df3569f8
longnh <longjava2024@gmail.com>
2025-03-17 14:24:15 +0700

    Auto-increment version to 1.0.8

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Định dạng phiên bản hợp lệ (X.Y.Z)
+# File lưu phiên bản hiện tại
 VERSION_FILE=".version"
 
-# Lấy phiên bản mới nhất từ Git tag
-LATEST_GIT_VERSION=$(git tag --sort=-v:refname | head -n 1)
+# Lấy phiên bản mới nhất từ Git tag (bỏ tiền tố "v" nếu có)
+LATEST_GIT_VERSION=$(git tag --sort=-v:refname | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
 
 # Nếu không có tag nào, gán mặc định 0.0.1
 if [ -z "$LATEST_GIT_VERSION" ]; then
@@ -13,18 +13,18 @@ fi
 
 # Kiểm tra file .version tồn tại
 if [ ! -f "$VERSION_FILE" ]; then
-  echo "VERSION=$LATEST_GIT_VERSION" > $VERSION_FILE
+  echo "VERSION=$LATEST_GIT_VERSION" > "$VERSION_FILE"
 fi
 
 # Đọc phiên bản hiện tại từ file
 CURRENT_VERSION=$(grep -oP '(?<=VERSION=)[0-9]+\.[0-9]+\.[0-9]+' "$VERSION_FILE")
 
-# Nếu không đọc được, gán bằng version từ Git
+# Nếu không đọc được, sử dụng phiên bản từ Git
 if [ -z "$CURRENT_VERSION" ]; then
   CURRENT_VERSION="$LATEST_GIT_VERSION"
 fi
 
-# Tách các phần của version (X.Y.Z)
+# Tách các phần của phiên bản (X.Y.Z)
 IFS='.' read -r MAJOR MINOR PATCH <<< "$LATEST_GIT_VERSION"
 
 # Tăng phiên bản: nếu PATCH đạt 9 thì tăng MINOR, nếu MINOR đạt 9 thì tăng MAJOR
